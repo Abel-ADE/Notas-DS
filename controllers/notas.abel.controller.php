9797d3d111696c3bf6fc98bfc72cdc23c137c5a8
Abel <abeliglesiasmoure@gmail.com>
2024-10-12 17:44:56 +0200

    validación de datos realizada

--- controllers/notas.abel.controller.php
+++ controllers/notas.abel.controller.php
@@ -8,17 +8,68 @@ if (!empty($_POST)) {
     $data['errors'] = checkForm($_POST);
 }
 
-function checkForm(array $data_json): array {
+function checkForm(array $data_json): array
+{
     $errors = [];
 
-    if(empty($data_json['input_json'])) {
-        $errors['input_json'] = 'El campo es obligatorio.';
+    //Valido que el usuario introduce algo en el textarea y no sea un número
+    if (empty($data_json['input_json'])) {
+        $errors['input_json'][] = 'El campo es obligatorio.';
+    } else {
+        if (is_numeric($data_json['input_json'])) {
+            $errors['input_json'][] = 'El campo debe ser un texto.';
+        }
+
+        $json = json_decode($data_json['input_json'], true);
+
+        //valido el json inicial si es válido y no está vacio
+        if (is_null($json)) {
+            $errors['input_json'][] = 'El campo debe ser un json válido.';
+        }else if(empty($json)){
+            $errors['input_json'][] = 'El json no puede estar vacio.';
+        }else {
+
+            foreach ($json as $asignatura => $alumnos) {
+                //Valido el texto de la asignatura
+                if(is_numeric($asignatura) || !is_string($asignatura)){
+                    $errors['input_json'][] = 'El campo asignatura debe ser un texto.';
+                }
+
+                //Valido el objeto alumnos de cada asignatura
+                if (!is_array($alumnos)){
+                    $errors['input_json'][] = 'La lista de alumnos debe ser un json.';
+                }else if (empty($alumnos)){
+                    $errors['input_json'][] = 'La lista de alumnos no puede estar vacia.';
+                }else{
+                    foreach ($alumnos as $alumno => $notas) {
+                        //Valido cada alumno
+                        if(is_numeric($alumno)){
+                            $errors['input_json'][] = 'El nombre del alumno debe ser un texto.';
+                        }elseif (empty($alumno)){
+                            $errors['input_json'][] = 'El nombre del alumno no puede estar vacio.';
+                        }
+
+                        //Valido las notas de cada alumno
+                        if (empty($notas)){
+                            $errors['input_json'][] = 'Las notas de cada alumno no pueden estar vacias';
+                        }else if (!is_array($notas)){
+                            $errors['input_json'][] = 'Las notas de cada alumno debe ser un array.';
+                        }else{
+                            foreach ($notas as $nota) {
+                                //Valido que cada nota sea un número
+                                if(!is_numeric($nota)){
+                                    $errors['input_json'][] = 'Cada nota debe ser un número.';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     return $errors;
 }
-
-
 
 
 include 'views/templates/header.php';
--- views/notas.abel.view.php
+++ views/notas.abel.view.php
@@ -20,7 +20,7 @@
                         <label for="input_json">Datos a analizar:</label>
                         <textarea class="form-control" name="input_json" id="input_json" rows="10"
                                   placeholder="Inserte el json a analizar"><?php echo isset($data['input_json']) ? $data['input_json'] : ''; ?></textarea>
-                        <p class="text-danger small"><?php echo isset($data['errors']['input_json']) ? $data['errors']['input_json'] : ''; ?></p>
+                        <p class="text-danger small"><?php echo isset($data['errors']['input_json']) ? implode('<br/>', $data['errors']['input_json']) : ''; ?></p>
                     </div>
                     <div class="mb-3">
                         <input type="submit" value="Enviar" name="enviar" class="btn btn-primary"/>
